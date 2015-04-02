3d0c12f9e2ccdfd2f55fa8525845bc512638122a
Nicolas Mesa <nicolasmesa@gmail.com>
2015-04-02 12:47:43 -0400

    Some support for expressions

--- Compiler/lex_yacc.py
+++ Compiler/lex_yacc.py
@@ -22,6 +22,7 @@ reserved = {
     'Shape' : 'K_SHAPE',
     'Rectangle' : 'K_RECTANGLE',
     'Point' : 'K_POINT',
+    'Text' : 'K_TEXT',
     'bool' : 'K_BOOL',
     'true' : 'K_TRUE',
     'false' : 'K_FALSE',
@@ -84,12 +85,12 @@ t_COMMA = r','
 # A regular expression rule with some action code
 def t_INTEGER(t):
     r'(\+|-)?[0-9]+'
-    t.value = int(t.value)    
+    #t.value = int(t.value) Not sure if this is needed. We are making a string as output
     return t
 
 def t_DOUBLE(t):
     r'(\+|-)?([0-9]\+\.?)'
-    t.value = float(t.value)    
+    #t.value = float(t.value)
     return t
 
 def t_ID(t):
@@ -118,6 +119,7 @@ def t_error(t):
 lexer = lex.lex()
 
 indent = 0
+
 
 def p_program(p):
     """
@@ -148,6 +150,7 @@ def p_statement(p):
 def p_compound_statement(p):
     """
     compound_statement : function_call_statement
+    compound_statement : variable_declaration
     """
     p[0] = p[1]
 
@@ -164,11 +167,105 @@ def p_function_call_statement(p):
 
 def p_parameter_list(p):
     """
-    parameter_list : parameter_list COMMA STRING
-    parameter_list : STRING
+    parameter_list : parameter_list COMMA expression
+    parameter_list : expression
     """
     if len(p) == 4:
         p[0] = p[1] + ", " + p[3]
+    else:
+        p[0] = p[1]
+
+def p_variable_declaration(p):
+    """
+    variable_declaration : pre_type_modifier type ID
+    variable_declaration : pre_type_modifier type ID ASSIGN expression
+    variable_declaration : ID ASSIGN expression
+    """
+    #@todo add all information about var. Process type modifier
+    if len(p) == 4:
+        if p[1] == "":
+            p[0] = p[3] + " = None"
+        elif p[1] == "list":
+            p[0] = p[3] + " = []"
+        elif p[1] == "dict":
+            p[0] = p[3] + " = {}"
+        elif p[1] == "set":
+            #@todo Don't know
+            p[0] = p[3] + " = @TODO initialize me"
+    elif len(p) == 6:
+        #@todo assign value
+        p[0] = p[3] + " = " + p[5]
+    else:
+        p[0] = p[3] + " = " + p[5]
+
+def p_pre_type_modifier(p):
+    """
+    pre_type_modifier :
+    pre_type_modifier : K_LIST
+    pre_type_modifier : K_DICT
+    pre_type_modifier : K_SET
+    """
+    if len(p) == 1:
+        p[0] = ""
+    else:
+        p[0] = p[1]
+
+def p_type(p):
+    """
+    type : K_INT
+    type : K_DOUBLE
+    type : K_STRING
+    type : K_BOOL
+    type : K_WINDOW
+    type : K_SHAPE
+    type : K_TRIANGLE
+    type : K_RECTANGLE
+    type : K_CIRCLE
+    type : K_TEXT
+    """
+    p[0] = p[1]
+
+
+def p_expression(p):
+    """
+    expression : string_expression
+    expression : unary_expression
+    """
+    p[0] = p[1]
+
+def p_string_expression(p):
+    """
+    string_expression : string_expression PLUS string_term
+    string_expression : STRING
+    """
+    if len(p) == 4:
+        p[0] = p[1] + " + " + p[3]
+    else:
+        p[0] = p[1]
+
+
+def p_string_term(p):
+    """
+    string_term : function_call_statement
+    string_term : STRING
+    """
+    p[0] = p[1]
+
+
+def p_unary_expression(p):
+    """
+    unary_expression : ID
+    unary_expression : STRING
+    unary_expression : INTEGER
+    unary_expression : function_call_statement
+    unary_expression : DOUBLE
+    unary_expression : K_TRUE
+    unary_expression : K_FALSE
+    """
+    if p[1] == "true":
+        p[0] = "True"
+    elif p[1] == "false":
+        p[0] = "False"
     else:
         p[0] = p[1]
 
--- Compiler/parsetab.py
+++ Compiler/parsetab.py
@@ -5,9 +5,9 @@ _tabversion = '3.2'
 
 _lr_method = 'LALR'
 
-_lr_signature = '\xa3-U\xfb\xbbL\x07\x88\x15GC\xd9\xbe\xfd\x870'
+_lr_signature = 'M\x07E\x01#\xa4h\x7f\x7f1\xf9e\xf7\x90hO'
     
-_lr_action_items = {'RPAREN':([7,9,10,13,],[8,-9,11,-8,]),'STRING':([7,12,],[9,13,]),'COMMA':([9,10,13,],[-9,12,-8,]),'LPAREN':([6,],[7,]),'ID':([0,1,3,4,5,8,11,],[-2,6,-3,-4,-5,-7,-6,]),'$end':([0,1,2,3,4,5,8,11,],[-2,-1,0,-3,-4,-5,-7,-6,]),}
+_lr_action_items = {'K_DOUBLE':([0,1,3,4,5,6,7,8,9,10,25,26,27,28,29,30,31,32,33,34,35,36,41,43,44,45,46,],[-2,-14,-4,-17,-5,-6,-16,-3,-15,13,-11,-28,-31,-29,-38,-37,-36,-39,-13,-34,-40,-8,-7,-12,-33,-32,-30,]),'K_TRUE':([23,24,39,42,],[32,32,32,32,]),'K_STRING':([0,1,3,4,5,6,7,8,9,10,25,26,27,28,29,30,31,32,33,34,35,36,41,43,44,45,46,],[-2,-14,-4,-17,-5,-6,-16,-3,-15,20,-11,-28,-31,-29,-38,-37,-36,-39,-13,-34,-40,-8,-7,-12,-33,-32,-30,]),'K_LIST':([0,1,3,5,6,8,25,26,27,28,29,30,31,32,33,34,35,36,41,43,44,45,46,],[-2,9,-4,-5,-6,-3,-11,-28,-31,-29,-38,-37,-36,-39,-13,-34,-40,-8,-7,-12,-33,-32,-30,]),'K_SET':([0,1,3,5,6,8,25,26,27,28,29,30,31,32,33,34,35,36,41,43,44,45,46,],[-2,4,-4,-5,-6,-3,-11,-28,-31,-29,-38,-37,-36,-39,-13,-34,-40,-8,-7,-12,-33,-32,-30,]),'PLUS':([26,27,36,41,44,45,46,],[40,-31,-8,-7,-33,-32,-30,]),'INTEGER':([23,24,39,42,],[31,31,31,31,]),'K_TRIANGLE':([0,1,3,4,5,6,7,8,9,10,25,26,27,28,29,30,31,32,33,34,35,36,41,43,44,45,46,],[-2,-14,-4,-17,-5,-6,-16,-3,-15,14,-11,-28,-31,-29,-38,-37,-36,-39,-13,-34,-40,-8,-7,-12,-33,-32,-30,]),'ASSIGN':([11,25,],[23,39,]),'K_CIRCLE':([0,1,3,4,5,6,7,8,9,10,25,26,27,28,29,30,31,32,33,34,35,36,41,43,44,45,46,],[-2,-14,-4,-17,-5,-6,-16,-3,-15,22,-11,-28,-31,-29,-38,-37,-36,-39,-13,-34,-40,-8,-7,-12,-33,-32,-30,]),'$end':([0,1,2,3,5,6,8,25,26,27,28,29,30,31,32,33,34,35,36,41,43,44,45,46,],[-2,-1,0,-4,-5,-6,-3,-11,-28,-31,-29,-38,-37,-36,-39,-13,-34,-40,-8,-7,-12,-33,-32,-30,]),'STRING':([23,24,39,40,42,],[27,27,27,44,27,]),'K_SHAPE':([0,1,3,4,5,6,7,8,9,10,25,26,27,28,29,30,31,32,33,34,35,36,41,43,44,45,46,],[-2,-14,-4,-17,-5,-6,-16,-3,-15,16,-11,-28,-31,-29,-38,-37,-36,-39,-13,-34,-40,-8,-7,-12,-33,-32,-30,]),'K_TEXT':([0,1,3,4,5,6,7,8,9,10,25,26,27,28,29,30,31,32,33,34,35,36,41,43,44,45,46,],[-2,-14,-4,-17,-5,-6,-16,-3,-15,17,-11,-28,-31,-29,-38,-37,-36,-39,-13,-34,-40,-8,-7,-12,-33,-32,-30,]),'K_INT':([0,1,3,4,5,6,7,8,9,10,25,26,27,28,29,30,31,32,33,34,35,36,41,43,44,45,46,],[-2,-14,-4,-17,-5,-6,-16,-3,-15,19,-11,-28,-31,-29,-38,-37,-36,-39,-13,-34,-40,-8,-7,-12,-33,-32,-30,]),'LPAREN':([11,34,47,],[24,24,24,]),'RPAREN':([24,26,27,28,29,30,31,32,34,35,36,37,38,41,44,45,46,48,],[36,-28,-31,-29,-38,-37,-36,-39,-34,-40,-8,41,-10,-7,-33,-32,-30,-9,]),'K_DICT':([0,1,3,5,6,8,25,26,27,28,29,30,31,32,33,34,35,36,41,43,44,45,46,],[-2,7,-4,-5,-6,-3,-11,-28,-31,-29,-38,-37,-36,-39,-13,-34,-40,-8,-7,-12,-33,-32,-30,]),'ID':([0,1,3,5,6,8,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,39,40,41,42,43,44,45,46,],[-2,11,-4,-5,-6,-3,-21,-19,-24,25,-23,-27,-25,-18,-20,-22,-26,34,34,-11,-28,-31,-29,-38,-37,-36,-39,-13,-34,-40,-8,34,47,-7,34,-12,-33,-32,-30,]),'K_BOOL':([0,1,3,4,5,6,7,8,9,10,25,26,27,28,29,30,31,32,33,34,35,36,41,43,44,45,46,],[-2,-14,-4,-17,-5,-6,-16,-3,-15,12,-11,-28,-31,-29,-38,-37,-36,-39,-13,-34,-40,-8,-7,-12,-33,-32,-30,]),'DOUBLE':([23,24,39,42,],[29,29,29,29,]),'K_RECTANGLE':([0,1,3,4,5,6,7,8,9,10,25,26,27,28,29,30,31,32,33,34,35,36,41,43,44,45,46,],[-2,-14,-4,-17,-5,-6,-16,-3,-15,18,-11,-28,-31,-29,-38,-37,-36,-39,-13,-34,-40,-8,-7,-12,-33,-32,-30,]),'K_WINDOW':([0,1,3,4,5,6,7,8,9,10,25,26,27,28,29,30,31,32,33,34,35,36,41,43,44,45,46,],[-2,-14,-4,-17,-5,-6,-16,-3,-15,21,-11,-28,-31,-29,-38,-37,-36,-39,-13,-34,-40,-8,-7,-12,-33,-32,-30,]),'COMMA':([26,27,28,29,30,31,32,34,35,36,37,38,41,44,45,46,48,],[-28,-31,-29,-38,-37,-36,-39,-34,-40,-8,42,-10,-7,-33,-32,-30,-9,]),'K_FALSE':([23,24,39,42,],[35,35,35,35,]),}
 
 _lr_action = { }
 for _k, _v in _lr_action_items.items():
@@ -16,7 +16,7 @@ for _k, _v in _lr_action_items.items():
       _lr_action[_x][_k] = _y
 del _lr_action_items
 
-_lr_goto_items = {'parameter_list':([7,],[10,]),'compound_statement':([1,],[4,]),'function_call_statement':([1,],[5,]),'program':([0,],[2,]),'statement':([1,],[3,]),'statement_list':([0,],[1,]),}
+_lr_goto_items = {'string_expression':([23,24,39,42,],[26,26,26,26,]),'string_term':([40,],[46,]),'parameter_list':([24,],[37,]),'unary_expression':([23,24,39,42,],[28,28,28,28,]),'compound_statement':([1,],[3,]),'expression':([23,24,39,42,],[33,38,43,48,]),'function_call_statement':([1,23,24,39,40,42,],[5,30,30,30,45,30,]),'program':([0,],[2,]),'statement':([1,],[8,]),'statement_list':([0,],[1,]),'pre_type_modifier':([1,],[10,]),'type':([10,],[15,]),'variable_declaration':([1,],[6,]),}
 
 _lr_goto = { }
 for _k, _v in _lr_goto_items.items():
@@ -26,13 +26,44 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> program","S'",1,None,None,None),
-  ('program -> statement_list','program',1,'p_program','lex_yacc.py',124),
-  ('statement_list -> <empty>','statement_list',0,'p_statement_list','lex_yacc.py',132),
-  ('statement_list -> statement_list statement','statement_list',2,'p_statement_list','lex_yacc.py',133),
-  ('statement -> compound_statement','statement',1,'p_statement','lex_yacc.py',143),
-  ('compound_statement -> function_call_statement','compound_statement',1,'p_compound_statement','lex_yacc.py',150),
-  ('function_call_statement -> ID LPAREN parameter_list RPAREN','function_call_statement',4,'p_function_call_statement','lex_yacc.py',156),
-  ('function_call_statement -> ID LPAREN RPAREN','function_call_statement',3,'p_function_call_statement','lex_yacc.py',157),
-  ('parameter_list -> parameter_list COMMA STRING','parameter_list',3,'p_parameter_list','lex_yacc.py',167),
-  ('parameter_list -> STRING','parameter_list',1,'p_parameter_list','lex_yacc.py',168),
+  ('program -> statement_list','program',1,'p_program','lex_yacc.py',126),
+  ('statement_list -> <empty>','statement_list',0,'p_statement_list','lex_yacc.py',134),
+  ('statement_list -> statement_list statement','statement_list',2,'p_statement_list','lex_yacc.py',135),
+  ('statement -> compound_statement','statement',1,'p_statement','lex_yacc.py',145),
+  ('compound_statement -> function_call_statement','compound_statement',1,'p_compound_statement','lex_yacc.py',152),
+  ('compound_statement -> variable_declaration','compound_statement',1,'p_compound_statement','lex_yacc.py',153),
+  ('function_call_statement -> ID LPAREN parameter_list RPAREN','function_call_statement',4,'p_function_call_statement','lex_yacc.py',159),
+  ('function_call_statement -> ID LPAREN RPAREN','function_call_statement',3,'p_function_call_statement','lex_yacc.py',160),
+  ('parameter_list -> parameter_list COMMA expression','parameter_list',3,'p_parameter_list','lex_yacc.py',170),
+  ('parameter_list -> expression','parameter_list',1,'p_parameter_list','lex_yacc.py',171),
+  ('variable_declaration -> pre_type_modifier type ID','variable_declaration',3,'p_variable_declaration','lex_yacc.py',180),
+  ('variable_declaration -> pre_type_modifier type ID ASSIGN expression','variable_declaration',5,'p_variable_declaration','lex_yacc.py',181),
+  ('variable_declaration -> ID ASSIGN expression','variable_declaration',3,'p_variable_declaration','lex_yacc.py',182),
+  ('pre_type_modifier -> <empty>','pre_type_modifier',0,'p_pre_type_modifier','lex_yacc.py',203),
+  ('pre_type_modifier -> K_LIST','pre_type_modifier',1,'p_pre_type_modifier','lex_yacc.py',204),
+  ('pre_type_modifier -> K_DICT','pre_type_modifier',1,'p_pre_type_modifier','lex_yacc.py',205),
+  ('pre_type_modifier -> K_SET','pre_type_modifier',1,'p_pre_type_modifier','lex_yacc.py',206),
+  ('type -> K_INT','type',1,'p_type','lex_yacc.py',215),
+  ('type -> K_DOUBLE','type',1,'p_type','lex_yacc.py',216),
+  ('type -> K_STRING','type',1,'p_type','lex_yacc.py',217),
+  ('type -> K_BOOL','type',1,'p_type','lex_yacc.py',218),
+  ('type -> K_WINDOW','type',1,'p_type','lex_yacc.py',219),
+  ('type -> K_SHAPE','type',1,'p_type','lex_yacc.py',220),
+  ('type -> K_TRIANGLE','type',1,'p_type','lex_yacc.py',221),
+  ('type -> K_RECTANGLE','type',1,'p_type','lex_yacc.py',222),
+  ('type -> K_CIRCLE','type',1,'p_type','lex_yacc.py',223),
+  ('type -> K_TEXT','type',1,'p_type','lex_yacc.py',224),
+  ('expression -> string_expression','expression',1,'p_expression','lex_yacc.py',231),
+  ('expression -> unary_expression','expression',1,'p_expression','lex_yacc.py',232),
+  ('string_expression -> string_expression PLUS string_term','string_expression',3,'p_string_expression','lex_yacc.py',238),
+  ('string_expression -> STRING','string_expression',1,'p_string_expression','lex_yacc.py',239),
+  ('string_term -> function_call_statement','string_term',1,'p_string_term','lex_yacc.py',249),
+  ('string_term -> STRING','string_term',1,'p_string_term','lex_yacc.py',250),
+  ('unary_expression -> ID','unary_expression',1,'p_unary_expression','lex_yacc.py',257),
+  ('unary_expression -> STRING','unary_expression',1,'p_unary_expression','lex_yacc.py',258),
+  ('unary_expression -> INTEGER','unary_expression',1,'p_unary_expression','lex_yacc.py',259),
+  ('unary_expression -> function_call_statement','unary_expression',1,'p_unary_expression','lex_yacc.py',260),
+  ('unary_expression -> DOUBLE','unary_expression',1,'p_unary_expression','lex_yacc.py',261),
+  ('unary_expression -> K_TRUE','unary_expression',1,'p_unary_expression','lex_yacc.py',262),
+  ('unary_expression -> K_FALSE','unary_expression',1,'p_unary_expression','lex_yacc.py',263),
 ]
