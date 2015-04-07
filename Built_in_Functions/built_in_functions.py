783aeb1dfc86d65e5e7cd81d498812ffe3e453ff
Ruibo-Li <rl2733@columbia.edu>
2015-04-07 16:25:55 -0400

    built-in functions

---
+++ Built_in_Functions/built_in_functions.py
@@ -0,0 +1,29 @@
+import graphics
+
+def getMouse(board):
+	board.getMouse()
+
+def render(board, shape):
+	shape.draw(board)
+
+def remove(board, shape):
+	
+
+def Window(width, height):
+	return GraphWin(width, height)
+
+def Rectangle(Point1, Point2):
+	return Rectangle(Point1, Point2)
+
+def Triangle(Point1, Point2, Point3):
+	vertices = [Point1, Point2, Point3]
+	return Polygon(vertices)
+
+def Circle(Center, Radius):
+	return Circle(Center, Radius)
+
+def Point(x, y):
+	return Point(x, y)
+
+def Line(Point1, Point2):
+	return Line(Point1, Point2)
