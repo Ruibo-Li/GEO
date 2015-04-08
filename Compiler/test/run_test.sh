18cf8fa1d9905a8e63a96c622e34699df0250783
Nicolas Mesa <nicolasmesa@gmail.com>
2015-04-07 23:03:11 -0400

    Test fix

--- Compiler/test/run_test.sh
+++ Compiler/test/run_test.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-python ../lex_yacc.py program3.geo > program3_actual.py
 
 for i in {1..3}
     do
-        program="program"+i+".geo"
-        benchmark="program"+i+".py"
-        actual="program"+i+"_actual.py"
-        python ../lex_yacc.py program > actual
+        program="program$i.geo"
+        benchmark="program$i.py"
+        actual="program$i_actual.py"
+        python ../lex_yacc.py $program > $actual
         if cmp -s benchmark actual
         then
 	        echo "test$i passes"
