2581fd5f9f76236acaf028c3bd60aaacf82f89d7
Nicolas Mesa <nicolasmesa@gmail.com>
2015-04-07 23:13:50 -0400

    Fix test

--- Compiler/test/run_test.sh
+++ Compiler/test/run_test.sh
@@ -5,7 +5,8 @@ for i in {1..3}
     do
         program="program$i.geo"
         benchmark="program$i.py"
-        actual="program$i_actual.py"
+        actual="program""$i""_actual.py"
+	echo $actual
         python ../lex_yacc.py $program > $actual
         if cmp -s benchmark actual
         then
