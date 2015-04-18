bc35a197a1d85d1d4eafa5191828f5552a8ab862
Yin Zhao <kidzhaoyin@gmail.com>
2015-04-18 15:13:56 -0400

    style

--- Built_in_Functions/built_in_classes.py
+++ Built_in_Functions/built_in_classes.py
@@ -1,70 +1,65 @@
-class G_shape(object):
+from graphics import *
+
+
+class GShape(object):
     def __init__(self):
+        self.geo = None
+
+    def draw(self, window):
+        self.geo.draw(window)
+
+    def remove(self, window):
+        self.geo.undraw(window)
 
 
-class G_triangle(G_shape):
+class GTriangle(GShape):
     def __init__(self, point1, point2, point3):
-        super(G_triangle, self).__init__()
+        super(GTriangle, self).__init__()
         vertices = [point1.geo, point2.geo, point3.geo]
         self.geo = Polygon(vertices)
-	self.vertices = [point1, point2, point3]
-
-    def draw(self, board):
-        self.geo.draw(board)
+        self.vertices = [point1, point2, point3]
 
     def get_triangle(self):
         return self.geo
 
 
-class G_rectangle(G_shape):
-    def __init__(self, point1, point2)
-        super(G_rectangle, self).__init__()
+class GRectangle(GShape):
+    def __init__(self, point1, point2):
+        super(GRectangle, self).__init__()
         vertices = [point1.geo, point2.geo]
         self.geo = Rectangle(point1, point2)
-	self.vertices = [point1, point2]
-
-    def draw(self, board):
-        self.geo.draw(board)
+        self.vertices = [point1, point2]
 
     def get_rectangle(self):
         return self.geo
 
 
-class G_circle(G_shape):
+class GCircle(GShape):
     def __init__(self, center, radius):
-        super(G_circle, self).__init__()
+        super(GCircle, self).__init__()
         self.geo = Circle(center.geo, radius)
-	self.center = center
-	self.radius = radius
-
-    def draw(self, board):
-        self.geo.draw(board)
+        self.center = center
+        self.radius = radius
 
     def get_circle(self):
         return self.geo
 
 
-class G_point:
+class GPoint:
     def __init__(self, x, y):
         self.geo = Point(x, y)
         self.x = x
         self.y = y
 
-    def draw(self, board):
-        self.geo.draw(board)
-
     def get_point(self):
         return self.geo
 
 
-class G_line:
-    def __init__(self, Point1, Point2):
-        vertices = [Point1.geo, Point2.geo]
-        self.geo = Line(Point1.geo, Point2.geo)
-        self.vertices = [Point1, Point2]
-
-    def draw(self, board):
-        self.geo.draw(board)
+class GLine:
+    def __init__(self, point1, point2):
+        vertices = [point1.geo, point2.geo]
+        self.geo = Line(point1.geo, point2.geo)
+        self.vertices = [point1, point2]
 
     def get_line(self):
         return self.geo
