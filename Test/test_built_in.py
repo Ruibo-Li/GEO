f09ea062dc15020ef5837133b7f4aca8ddc496cf
Di Ruan <ruandi88@gmail.com>
2015-04-18 13:47:17 -0400

    add first test

--- Test/test_built_in.py
+++ Test/test_built_in.py
@@ -1,9 +1,14 @@
+import sys
+import os.path
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from Built_in_Functions.built_in_functions import *
 
 
 def main():
+    # test Window
     Window(100, 100)
 
-
-def test(name, module, function_name, input, output):
-    methodToCall = getattr(module, function_name)
+    # test render
+    Board board = createBoard()
+    Shape shape = createShape()
+    render(board, shape)
