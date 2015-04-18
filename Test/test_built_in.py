428825898dfa5e1781d059ebc4566ef431b19b9f
Di Ruan <ruandi88@gmail.com>
2015-04-18 15:51:52 -0400

    add more tests

--- Test/test_built_in.py
+++ Test/test_built_in.py
@@ -6,9 +6,28 @@ from Built_in_Functions.built_in_functions import *
 
 def main():
     # test Window
-    Window(100, 100)
+    board = createWindow(100, 100)
+    board.getMouse()
 
-    # test render
-    Board board = createBoard()
-    Shape shape = createShape()
-    render(board, shape)
+    # test Point
+    point = createPoint(5, 5)
+    render(board, point)
+
+    # test Triangle
+    p21 = createPoint(5, 10)
+    p22 = createPoint(10, 10)
+    p23 = createPoint(8, 15)
+    tr1 = createTriangle(p21, p22, p23)
+    render(board, tr1)
+
+    # test Circle
+    p31 = createPoint(5, 10)
+    circle = createCircle(p31, 20)
+    render(board, circle)
+
+    # test Line
+    p41 = createPoint(6, 7)
+    p42 = createPoint(10, 12)
+    line = createLine(p41, p42)
+    render(board, line)
+
