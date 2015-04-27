6917331bacc13b63e8f256340688ee41249661bf
Yin Zhao <kidzhaoyin@gmail.com>
2015-04-27 17:47:34 -0400

    added string functions

--- Built_in_Functions/built_in_functions.py
+++ Built_in_Functions/built_in_functions.py
@@ -114,34 +114,114 @@ def cast_shape(shape):
         return GLine(shape)
     return None
 
+
 # list operation functions
 def listAppend(ls, x):
     ls.append(x)
 
+
 def listExtend(ls1, ls2):
     ls1.extend(ls2)
+
 
 def listInsert(ls, i, x):
     ls.insert(i, x)
 
+
 def listRemove(ls, x):
     ls.remove(x)
+
 
 def listPop(ls, i=0):
     return ls.pop(i)
 
+
 def listIndex(ls, x):
     return ls.index(x)
+
 
 def listCount(ls, x):
     return ls.count(x)
 
+
 def listSort(ls):
     ls.sort()
+
 
 def listReverse(ls):
     ls.reverse()
 
-def printf(str):
-    print str
+
+def printf(st):
+    print st
     return None
+
+
+def split(st):
+    return st.split()
+
+
+def splitBy(st, sep):
+    return st.split(sep)
+
+
+def strip(st):
+    return st.strip()
+
+
+def stripBy(st, sep):
+    return st.strip(sep)
+
+
+def replaceString(st, old, new):
+    return st.replace(old, new)
+
+
+def findSubstring(st, sub):
+    return st.find(sub)
+
+
+def encode(st, encoding):
+    return st.encode(encoding)
+
+
+def index(st, sub):
+    return st.index(sub)
+
+
+def isDigit(st):
+    return st.isdigit()
+
+
+def isUpper(st):
+    return st.isupper()
+
+
+def isLower(st):
+    return st.islower()
+
+
+def lower(st):
+    return st.lower()
+
+
+def upper(st):
+    return st.upper()
+
+
+def joinString(sep, iterable):
+    return sep.join(iterable)
+
+
+def partition(st, sep):
+    return st.partition(sep)
+
+
+def isNumeric(st):
+    return st.isnumeric()
+
+
+def isDecimal(st):
+    return st.isdecimal()
+
+
